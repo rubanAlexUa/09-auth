3eba3389bd1f06d791bd3c87d59cfdcd1f068a3b
Alex <rubashka2021@gmail.com>
2025-09-21 12:04:22 +0300

    add all

--- app/(private routes)/notes/[id]/NoteDetails.client.tsx
+++ app/(private routes)/notes/[id]/NoteDetails.client.tsx
@@ -1,6 +1,6 @@
 "use client";
 import css from "./NoteDetails.module.css";
-import { fetchNoteById } from "@/lib/api/api";
+import { fetchNoteById } from "@/lib/api/clientApi";
 
 import { useQuery } from "@tanstack/react-query";
 import { useParams, useRouter } from "next/navigation";
